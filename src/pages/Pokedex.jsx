c5f1a3bd2c1221f6f38734416b40a90123ad3696
Michel <michelantunezqueiroz@gmail.com>
2026-01-07 22:45:24 -0300

    Instalação do Axios e primeira chamada à API

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,7 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  const getPokemons = () => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0")
+      .then((response) => setPokemons(response.data.results))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
       <h1>Pokédex</h1>
+      {pokemons.map((pokemon) => (
+        <div key={pokemon.name}>{pokemon.name}</div>
+      ))}
     </>
   );
 }
