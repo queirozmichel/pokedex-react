9e3f55df81ebf56b8ef31720061d7443fc021289
Michel <michelantunezqueiroz@gmail.com>
2026-01-08 22:12:19 -0300

    Trazendo os nomes dos pokémons

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,5 +1,7 @@
+import { Grid } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import PokemonCard from "../components/PokemonCard";
 
 export default function Pokedex() {
   const [pokemons, setPokemons] = useState([]);
@@ -17,10 +19,13 @@ export default function Pokedex() {
 
   return (
     <>
-      <h1>Pokédex</h1>
-      {pokemons.map((pokemon) => (
-        <div key={pokemon.name}>{pokemon.name}</div>
-      ))}
+      <Grid container spacing={2} sx={{ padding: "1rem" }}>
+        {pokemons.map((pokemon, index) => (
+          <Grid size={{ xs: 12, sm: 6, md: 4, lg: 2 }} key={index}>
+            <PokemonCard pokemon={pokemon} />
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 }
